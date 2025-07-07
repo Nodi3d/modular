5b8fa275b2f7135c3aa1386eb5225d2cd21addb6
mattatz <masatatsu.nakamura@gmail.com>
2025-07-07 15:43:10 +0900

    Improve parameter updates for nozzle.

--- showcase/nozzle/src/components/ui/PropertyPanel.tsx
+++ showcase/nozzle/src/components/ui/PropertyPanel.tsx
@@ -1,33 +1,49 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react";
 
-import { Tooltip } from "react-tooltip"
-import { motion } from "framer-motion"
+import { Tooltip } from "react-tooltip";
+import { motion } from "framer-motion";
 
-import {  useModularStore } from "@/stores/modular"
-import { Material, useNozzleStore } from "@/stores/nozzle"
-import { useSettingsStore } from "@/stores/settings"
-import CustomSlider from "./CustomSlider"
+import { useModularStore } from "@/stores/modular";
+import { Material, useNozzleStore } from "@/stores/nozzle";
+import { useSettingsStore } from "@/stores/settings";
+import CustomSlider from "./CustomSlider";
+
+const useParameterSync = (
+  id: string,
+  value: number,
+  // eslint-disable-next-line 
+  callback: (id: string, value: number) => void
+) => {
+  useEffect(() => {
+    callback(id, value);
+  }, [id, value, callback]);
+};
 
 export const PropertyPanel: React.FC = () => {
-  const {updateNodeProperty,nodeIds} = useModularStore()
-  const {length,outerSize,tipInnerSize,tipOuterSize,needleLength,updateNozzle,material} = useNozzleStore()
-  const {propertyHover, setPropertyHover} = useSettingsStore()
-  
-  useEffect(()=>{
-    if (nodeIds.length&&nodeIds.outerSize&&nodeIds.tipInnerSize&&nodeIds.tipOuterSize&&nodeIds.needleLength){
-      updateNodeProperty(nodeIds.length, length)
-      updateNodeProperty(nodeIds.outerSize, outerSize)
-      updateNodeProperty(nodeIds.tipInnerSize, tipInnerSize)
-      updateNodeProperty(nodeIds.tipOuterSize, tipOuterSize)
-      updateNodeProperty(nodeIds.needleLength, needleLength)
-    }
-  },[length,outerSize,tipInnerSize,tipOuterSize,needleLength,nodeIds])  
+  const { updateNodeProperty, nodeIds } = useModularStore();
+  const {
+    length,
+    outerSize,
+    tipInnerSize,
+    tipOuterSize,
+    needleLength,
+    updateNozzle,
+    material,
+  } = useNozzleStore();
+  const { propertyHover, setPropertyHover } = useSettingsStore();
+
+  useParameterSync(nodeIds.length, length, updateNodeProperty);
+  useParameterSync(nodeIds.outerSize, outerSize, updateNodeProperty);
+  useParameterSync(nodeIds.tipInnerSize, tipInnerSize, updateNodeProperty);
+  useParameterSync(nodeIds.tipOuterSize, tipOuterSize, updateNodeProperty);
+  useParameterSync(nodeIds.needleLength, needleLength, updateNodeProperty);
 
   return (
     <>
       <motion.div
         className={`absolute h-auto top-2 md:top-8 left-0 md:left-8 w-full md:w-64 p-4 py-0 md:py-2 z-20 rounded-md backdrop-blur-sm flex flex-col gap-2 items-center justify-center font-serif text-content-h-a`}
-        layout>
+        layout
+      >
         <div className="grid grid-cols-[minmax(0,_1fr)_repeat(2,_minmax(62px,_1fr))] gap-2 items-center w-full">
           <label className="text-sm" htmlFor="material">
             材質
@@ -35,7 +51,8 @@ export const PropertyPanel: React.FC = () => {
           <div className="t-tabs col-span-2 text-sm">
             <button
               className={material === "bronze" ? "t-tab-active" : ""}
-              onClick={() => updateNozzle({ material: "bronze" as Material })}>
+              onClick={() => updateNozzle({ material: "bronze" as Material })}
+            >
               真鍮
             </button>
             <span className="bg-transparent"></span>
@@ -43,7 +60,8 @@ export const PropertyPanel: React.FC = () => {
               className={material === "high-wear-steel" ? "t-tab-active" : ""}
               onClick={() =>
                 updateNozzle({ material: "high-wear-steel" as Material })
-              }>
+              }
+            >
               高耐摩耗鋼
             </button>
           </div>
@@ -55,7 +73,8 @@ export const PropertyPanel: React.FC = () => {
           }
           onMouseLeave={() =>
             setPropertyHover({ ...propertyHover, length: false })
-          }>
+          }
+        >
           <label className="text-sm" htmlFor="length">
             全長
           </label>
@@ -64,7 +83,7 @@ export const PropertyPanel: React.FC = () => {
               type="text"
               value={length}
               onChange={(e) => {
-                updateNozzle({ length: Number(e.target.value) })
+                updateNozzle({ length: Number(e.target.value) });
               }}
               className="t-input bg-content-xxl-a w-full"
             />
@@ -78,7 +97,7 @@ export const PropertyPanel: React.FC = () => {
             step={0.1}
             value={length}
             onChange={(value) => {
-              updateNozzle({ length: value })
+              updateNozzle({ length: value });
               // updateNodeProperty(nodeIds.holeSize, value)
             }}
           />
@@ -90,7 +109,8 @@ export const PropertyPanel: React.FC = () => {
           }
           onMouseLeave={() =>
             setPropertyHover({ ...propertyHover, outerSize: false })
-          }>
+          }
+        >
           <label className="text-sm" htmlFor="maxOuterDiameter">
             最大外径
           </label>
@@ -99,7 +119,7 @@ export const PropertyPanel: React.FC = () => {
               type="text"
               value={outerSize}
               onChange={(e) => {
-                updateNozzle({ outerSize: Number(e.target.value) })
+                updateNozzle({ outerSize: Number(e.target.value) });
               }}
               className="t-input bg-content-xxl-a w-full"
             />
@@ -113,7 +133,7 @@ export const PropertyPanel: React.FC = () => {
             step={0.1}
             value={outerSize}
             onChange={(value) => {
-              updateNozzle({ outerSize: value })
+              updateNozzle({ outerSize: value });
               // updateNodeProperty(nodeIds.nozzleSize, value)
             }}
           />
@@ -125,7 +145,8 @@ export const PropertyPanel: React.FC = () => {
           }
           onMouseLeave={() =>
             setPropertyHover({ ...propertyHover, tipInnerSize: false })
-          }>
+          }
+        >
           <label className="text-sm" htmlFor="tipInnerDiameter">
             先端部内径
           </label>
@@ -134,7 +155,7 @@ export const PropertyPanel: React.FC = () => {
               type="text"
               value={tipInnerSize}
               onChange={(e) => {
-                updateNozzle({ tipInnerSize: Number(e.target.value) })
+                updateNozzle({ tipInnerSize: Number(e.target.value) });
               }}
               className="t-input bg-content-xxl-a w-full"
             />
@@ -148,7 +169,7 @@ export const PropertyPanel: React.FC = () => {
             step={0.1}
             value={tipInnerSize}
             onChange={(value) => {
-              updateNozzle({ tipInnerSize: value })
+              updateNozzle({ tipInnerSize: value });
               // updateNodeProperty(nodeIds.nozzleSize, value)
             }}
           />
@@ -160,7 +181,8 @@ export const PropertyPanel: React.FC = () => {
           }
           onMouseLeave={() =>
             setPropertyHover({ ...propertyHover, tipOuterSize: false })
-          }>
+          }
+        >
           <label className="text-sm" htmlFor="tipOuterDiameter">
             先端部外径
           </label>
@@ -169,7 +191,7 @@ export const PropertyPanel: React.FC = () => {
               type="text"
               value={tipOuterSize}
               onChange={(e) => {
-                updateNozzle({ tipOuterSize: Number(e.target.value) })
+                updateNozzle({ tipOuterSize: Number(e.target.value) });
               }}
               className="t-input bg-content-xxl-a w-full"
             />
@@ -183,7 +205,7 @@ export const PropertyPanel: React.FC = () => {
             step={0.1}
             value={tipOuterSize}
             onChange={(value) => {
-              updateNozzle({ tipOuterSize: value })
+              updateNozzle({ tipOuterSize: value });
               // updateNodeProperty(nodeIds.nozzleSize, value)
             }}
           />
@@ -195,7 +217,8 @@ export const PropertyPanel: React.FC = () => {
           }
           onMouseLeave={() =>
             setPropertyHover({ ...propertyHover, needleLength: false })
-          }>
+          }
+        >
           <label className="text-sm" htmlFor="tipNeedleLength">
             先端針長さ
           </label>
@@ -204,7 +227,7 @@ export const PropertyPanel: React.FC = () => {
               type="text"
               value={needleLength}
               onChange={(e) => {
-                updateNozzle({ needleLength: Number(e.target.value) })
+                updateNozzle({ needleLength: Number(e.target.value) });
               }}
               className="t-input bg-content-xxl-a w-full"
             />
@@ -218,16 +241,24 @@ export const PropertyPanel: React.FC = () => {
             step={0.01}
             value={needleLength}
             onChange={(value) => {
-              updateNozzle({ needleLength: value })
+              updateNozzle({ needleLength: value });
               // updateNodeProperty(nodeIds.nozzleSize, value)
             }}
           />
         </div>
         <a
           className="border-[1px] border-content-h-a px-4 py-2 text-sm hover:bg-content-h-a hover:text-content-dark-h-a w-full mt-2 transition-all text-center"
-          href={`https://docs.google.com/forms/d/e/1FAIpQLSf5L-INqDqmH0yHtx1aOoFguA1MpkODBXZIua-5Bm2t3KBU6Q/viewform?usp=pp_url&entry.1225353679=${encodeURIComponent(length)}&entry.11693644=${encodeURIComponent(outerSize)}&entry.1455151050=${encodeURIComponent(tipInnerSize)}&entry.300096431=${encodeURIComponent(tipOuterSize)}&entry.1298909336=${encodeURIComponent(needleLength)}`}
+          href={`https://docs.google.com/forms/d/e/1FAIpQLSf5L-INqDqmH0yHtx1aOoFguA1MpkODBXZIua-5Bm2t3KBU6Q/viewform?usp=pp_url&entry.1225353679=${encodeURIComponent(
+            length
+          )}&entry.11693644=${encodeURIComponent(
+            outerSize
+          )}&entry.1455151050=${encodeURIComponent(
+            tipInnerSize
+          )}&entry.300096431=${encodeURIComponent(
+            tipOuterSize
+          )}&entry.1298909336=${encodeURIComponent(needleLength)}`}
           target={"_blank"}
-          >
+        >
           ORDER
         </a>
 
@@ -247,5 +278,5 @@ export const PropertyPanel: React.FC = () => {
         />
       </motion.div>
     </>
-  )
-}
+  );
+};
